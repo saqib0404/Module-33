9fa3ff795cbc1333c76f9e9d6122340b7adaeb26
Saqib Ahmad <you@example.com>
2022-08-28 06:45:55 +0600

    countr btn dynamically made and added

--- js/country.js
+++ js/country.js
@@ -1,18 +1,33 @@
-const loadCountries =() => {
+const loadCountries = () => {
     fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data => desplayCountry(data))
+        .then(res => res.json())
+        .then(data => desplayCountry(data))
 }
-const desplayCountry = countries =>{
+const desplayCountry = countries => {
     const countriesContainer = document.getElementById('countries');
-    countries.map(country =>{
+    countries.map(country => {
         console.log(country);
         const countryDiv = document.createElement('div');
-        countryDiv.innerHTML=`
+        countryDiv.classList.add('countries')
+        countryDiv.innerHTML = `
         <h3>Name: ${country.name.common}</h3>
         <h4>Capital: ${country.capital ? country.capital[0] : "ERROR!!!!!!!!!!!"}</h4>
+        <button onclick="loadCountryDetails('${country.cca2}')">See Details</button>
         `
         countriesContainer.appendChild(countryDiv)
     })
 }
-loadCountries()
+const loadCountryDetails = (code) => {
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetails(data[0]))
+}
+const displayCountryDetails = (details) => {
+    const countryDetails = document.getElementById('country-details');
+    countryDetails.innerHTML =`
+    <h2>Country Details: ${details.name.common}</h2>
+    <img src="${details.flags.png}">
+    `
+}
+loadCountries();
